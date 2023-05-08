31ddd0505d081f97cf1e1e7f1630c3283db103e0
Diego Feldman <diegofeldman@gmail.com>
2023-05-08 20:21:03 -0300

    Actualización

---
+++ Enc37_Ejercicio04_Collections_Cine/src/Entidades/Pelicula.java
@@ -0,0 +1,52 @@
+/*
+Control
+ */
+package Entidades;
+
+/**
+ *
+ * @author diego
+ */
+public class Pelicula {
+    private String titulo;
+    private String director;
+    private float duracion;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(String titulo, String director, float duracion) {
+        this.titulo = titulo;
+        this.director = director;
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public float getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(float duracion) {
+        this.duracion = duracion;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" + "titulo=" + titulo + ", director=" + director + ", duracion=" + duracion + '}';
+    }
+}
