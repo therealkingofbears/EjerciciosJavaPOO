4fe4e47f2e192970c5d7a2d4e33e653898d3412f
Diego Feldman <diegofeldman@gmail.com>
2023-05-10 16:49:19 -0300

    Actualización

---
+++ Enc39Ej00_Relaciones_Persona/src/Entidades/DNI.java
@@ -0,0 +1,47 @@
+/*
+Realiza un programa en donde una clase Persona tenga como atributo nombre, apellido y un
+objeto de clase Dni. La clase Dni tendrá como atributos la serie (carácter) y número. Prueba
+acceder luego a los atributos del dni de la persona creando objetos y jugando desde el main.
+ */
+package Entidades;
+
+
+/**
+ *
+ * @author diego
+ */
+public class DNI {
+    private String serie;
+    private int numero;
+
+    public DNI() {
+    }
+
+    public DNI(String serie, int numero) {
+        this.serie = serie;
+        this.numero = numero;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "DNI{" + "serie=" + serie + ", numero=" + numero + '}';
+    }
+    
+    
+}
