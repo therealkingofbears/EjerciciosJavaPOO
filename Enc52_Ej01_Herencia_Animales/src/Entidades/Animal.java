a5ad8ae2bda93c94a4d09c203749c1e1d6510e19
Diego Feldman <diegofeldman@gmail.com>
2023-06-02 16:16:44 -0300

    Actualización

---
+++ Enc52_Ej01_Herencia_Animales/src/Entidades/Animal.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ La clase Animal tendrá como atributos el nombre, alimento, edad y raza del Animal.
+ */
+public class Animal {
+    protected String nombre;
+    protected String alimento;
+    protected Integer edad;
+    protected String raza;
+
+    public Animal() {
+    }
+
+    public Animal(String nombre, String alimento, Integer edad, String raza) {
+        this.nombre = nombre;
+        this.alimento = alimento;
+        this.edad = edad;
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAlimento() {
+        return alimento;
+    }
+
+    public void setAlimento(String alimento) {
+        this.alimento = alimento;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "nombre=" + nombre + ", alimento=" + alimento + ", edad=" + edad + ", raza=" + raza + '}';
+    }
+    
+    public void Alimentarse() {
+        
+    }
+}
