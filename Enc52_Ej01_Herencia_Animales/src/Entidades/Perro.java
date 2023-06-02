b7d5c98caaf5c13c2d49d803839214a13a8c1679
Diego Feldman <diegofeldman@gmail.com>
2023-06-02 16:16:49 -0300

    Actualización

---
+++ Enc52_Ej01_Herencia_Animales/src/Entidades/Perro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author diego
+ */
+public class Perro extends Animal {
+    
+    public Perro(String nombre, String alimento, Integer edad, String raza) {
+        super(nombre, alimento, edad, raza);
+    }
+}
