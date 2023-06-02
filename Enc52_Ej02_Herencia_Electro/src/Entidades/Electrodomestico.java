a6b12affe5137d0c143b7eea5307faae54d9308c
Diego Feldman <diegofeldman@gmail.com>
2023-06-02 16:15:55 -0300

    Actualización

---
+++ Enc52_Ej02_Herencia_Electro/src/Entidades/Electrodomestico.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+public class Electrodomestico {
+    protected double precio;
+    protected String color;
+    protected char consumoEnergetico;
+    protected int peso;
+
+    public Electrodomestico() {
+        this.precio = 0;
+        this.color = "blanco";
+        this.consumoEnergetico = 'F';
+        this.peso = 0;
+    }
+
+    public Electrodomestico(double precio, String color, char consumoEnergetico, int peso) {
+        this.precio = precio;
+        this.color = color;
+        this.consumoEnergetico = consumoEnergetico;
+        this.peso = peso;
+    }
+
+    // Getters y Setters
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public char getConsumoEnergetico() {
+        return consumoEnergetico;
+    }
+
+    public void setConsumoEnergetico(char consumoEnergetico) {
+        this.consumoEnergetico = consumoEnergetico;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+}
+
