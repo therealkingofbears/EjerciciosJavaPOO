79d98ee040d6f462841ccca10b20594bc4b6ef71
Diego Feldman <diegofeldman@gmail.com>
2023-06-02 16:17:06 -0300

    Actualización

---
+++ Enc52_Ej01_Herencia_Animales/src/Servicio/GatoServicio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Servicio;
+
+import Entidades.Gato;
+
+/**
+ *
+ * @author diego
+ */
+public class GatoServicio extends Gato {
+
+    public GatoServicio(String nombre, String alimento, Integer edad, String raza) {
+        super(nombre, alimento, edad, raza);
+    }
+    
+    @Override
+    public void Alimentarse() {
+        System.out.println("Come Whiskas");
+    }
+}
